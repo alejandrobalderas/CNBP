7c0f04f1623a710805bf35a63ed6bc32070d17be
Alejandro Balderas <31583496+alejandrobalderas@users.noreply.github.com>
2018-09-25 14:41:18 +0200

    node js commit and all

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-var static = require('node-static');
-
+const static = require('node-static');
+const ws = require('ws');
+const http = require('http');
 //
 // Create a node-static server instance to serve the './public' folder
 //
-var file = new static.Server('./public');
+const file = new static.Server('./public');
+const server = http.createServer(function (request, response) {
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        file.serve(request, response);
-    }).resume();
-}).listen(8080);
+      request.addListener('end', function () {
+          //
+          // Serve files!
+          //
+          file.serve(request, response);
+      }).resume();
+
+});
+
+const wss = new ws.Server({ server : server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+
+
+server.listen(8080);
