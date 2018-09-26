7b1f2df24e0c1cd3c947466620c203d9bb1b6c7a
Alejandro Balderas <31583496+alejandrobalderas@users.noreply.github.com>
2018-09-26 15:56:34 +0200

    Added Chatbot integration

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require('dotenv').config();
 const static = require('node-static');
 const ws = require('ws');
 const http = require('http');
+const ConversationV1 = require('watson-developer-cloud/conversation/v1');
+
+var conversation = new ConversationV1({
+    version: '2018-09-20',
+    username: process.env.WATSON_API_USERNAME,
+    password: process.env.WATSON_API_PASSWORD,
+    url: 'https://gateway.watsonplatform.net/assistant/api'
+});
+
 //
 // Create a node-static server instance to serve the './public' folder
 //
@@ -19,12 +29,41 @@ const server = http.createServer(function (request, response) {
 const wss = new ws.Server({ server : server });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+
+  conversation.message({
+    workspace_id: process.env.WATSON_WORKSPACE_ID,
+    input: {'text': ``}
+  },  function(err, response) {
+    if (err) {
+      console.log('error:', err);
+    } else {
+      //console.log(JSON.stringify(response, null, 2));
+      ws.send(JSON.stringify(response))
+    }
+
   });
 
-  ws.send('something');
-});
+  ws.on('message', function incoming(data) {
+    console.log('received: %s', data);
 
+    data = JSON.parse(data);
+
+    conversation.message({
+      workspace_id: process.env.WATSON_WORKSPACE_ID,
+      input: {'text': data.text},
+      context : data.context
+    },  function(err, response) {
+      if (err) {
+        console.log('error:', err);
+      } else {
+        //console.log(JSON.stringify(response, null, 2));
+        ws.send(JSON.stringify(response))
+      }
+
+    });
+
+    //ws.send('something');
+  });
+});
 
 server.listen(8080);
